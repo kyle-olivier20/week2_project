2f4c7190cbceffc741fdd5dbee5b8620b3a3ecea
Levi makwei <levi.d.makwei@gmail.com>
2022-07-06 14:19:21 +0000

    adding tests
    Co-authored-by: Kyle Olivier <kyleolivier12@gmail.com>

--- finder.py
+++ finder.py
@@ -1,13 +1,16 @@
 import requests
 import urllib.parse
 
-requests.get("https://api.trace.moe/search?url={}".format(urllib.parse.quote_plus("https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg"))).json()
+url = "https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg"
+api_url='https://api.trace.moe/search'
 
-requests.post("https://api.trace.moe/search", data=open("demo.jpg", "rb"),headers={"Content-Type": "image/jpeg"}).json()
+requests.get("{}?url={}".format(api_url,urllib.parse.quote_plus(url))).json()
 
-requests.post("https://api.trace.moe/search", files={"image": open("demo.jpg", "rb")}).json()
+requests.post(f"{api_url}", data=open("demo.jpg", "rb"),headers={"Content-Type": "image/jpeg"}).json()
 
-requests.get("https://api.trace.moe/search?cutBorders&url={}".format(urllib.parse.quote_plus("https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg"))).json()
+requests.post(f"{api_url}", files={"image": open("demo.jpg", "rb")}).json()
 
-requests.get("https://api.trace.moe/search?anilistID=1&url={}".format(urllib.parse.quote_plus("https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg"))).json()
+requests.get("{}?cutBorders&url={}".format(api_url,urllib.parse.quote_plus(url))).json()
+
+requests.get("{}?anilistID=1&url={}".format(api_url,urllib.parse.quote_plus(url))).json()
 
