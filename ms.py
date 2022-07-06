fdbae80f9cb3e9ceab84a73572ef401eccde0d11
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-06 21:22:49 +0000

    Got all the needed information

--- ms.py
+++ ms.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# to get city input
 city = input()
 
 response = requests.get('https://api.teleport.org/api/cities/?search={}'.format(city))
@@ -14,11 +15,23 @@ jsonObject = response.json()
             #city:item : object
                 #href : string
 
-
-if '_embedded' in jsonObject:
+# to get the geonameid
+'''if '_embedded' in jsonObject:
     if 'city:search-results' in jsonObject['_embedded']:
         if len(jsonObject['_embedded']['city:search-results']) > 1:
-            print(jsonObject['_embedded']['city:search-results'][0]['_links']['city:item']['href'][46:53])
+            print(jsonObject['_embedded']['city:search-results'][0]['_links']['city:item']['href'][36:53])'''
+
+#to get the correct city in mind
+p = jsonObject['_embedded']['city:search-results'][0]['_links']['city:item']['href'][46:53]
+r = requests.get('https://api.teleport.org/api/cities/geonameid%3A{}'.format(p))
+
+# to get full name and population
+jsonObj2 = r.json()
+'''print(json.dumps(r.json(), indent=4))'''
+print(jsonObj2['full_name'])
+print(jsonObj2['population'])
+
+
 
 
 
@@ -26,11 +39,11 @@ if '_embedded' in jsonObject:
 # Get 'https://api.teleport.org/api/cities/?search=CITY' CHECK
 # Parse for geonameid:NUM  CHECK
 # return geonameid:NUM CHECK
-# Get https://api.teleport.org/api/cities/geonameid%3ACITYID/
-# %3AC is :
-# Parse for population
-# Parse for name
-# Display it to user
+# Get https://api.teleport.org/api/cities/geonameid%3ACITYID/ CHECK
+# %3AC is : 
+# Parse for population CHECK
+# Parse for name CHECK
+# Display it to user CHECK
 
 # Shove city name and population into database
 # On request, give user top 5 populous cities
