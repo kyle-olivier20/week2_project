c9b9d486ceb0b7285bb452528e5b4f006cd36b61
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-06 20:40:01 +0000

    Got geonameid number

--- ms.py
+++ ms.py
@@ -1,9 +1,11 @@
 import requests
 import json
 
-response = requests.get('https://api.teleport.org/api/cities/?search=los%20angeles')
+city = input()
 
-print(json.dumps(response.json(), indent=4))
+response = requests.get('https://api.teleport.org/api/cities/?search={}'.format(city))
+
+'''print(json.dumps(response.json(), indent=4))'''
 
 jsonObject = response.json()
 #_embedded: object
@@ -16,13 +18,14 @@ jsonObject = response.json()
 if '_embedded' in jsonObject:
     if 'city:search-results' in jsonObject['_embedded']:
         if len(jsonObject['_embedded']['city:search-results']) > 1:
+            print(jsonObject['_embedded']['city:search-results'][0]['_links']['city:item']['href'][46:53])
 
-print(jsonObject['_embedded']['city:search-results'][0]['_links']['city:item']['href'])
 
-# Get city input from user
-# Get 'https://api.teleport.org/api/cities/?search=CITY'
-# Parse for geonameid:NUM
-# return geonameid:NUM
+
+# Get city input from user CHECK
+# Get 'https://api.teleport.org/api/cities/?search=CITY' CHECK
+# Parse for geonameid:NUM  CHECK
+# return geonameid:NUM CHECK
 # Get https://api.teleport.org/api/cities/geonameid%3ACITYID/
 # %3AC is :
 # Parse for population
